acce9c2b23c9f106a3c919ac67a585e3fee3f0f7
cdisciascio <cecidisi@gmail.com>
2015-10-15 16:12:42 +0200

    under construction

---
+++ model/scoreExtractor.js
@@ -0,0 +1,62 @@
+window.ScoreExtractor = (function(){
+
+    var _this;
+
+    function ScoreExtractor() {
+        _this = this;
+        this.items = [];
+        this.normalizedScores = [];
+        this.scoreSet = [];
+        this.scoreSetDict = {};
+    }
+
+
+    ScoreExtractor.prototype = {
+
+        addItem: function(_item){
+            this.items.push(_item);
+            return this;
+        },
+
+        run: function(){
+            // extarct score set
+            this.scoreSet = [];
+            this.scoreSetDict = {};
+
+            this.items.forEach(function(d, i){
+                if(Array.isArray(d.scores)) {
+                    d.scores.forEach(function(s){});
+                }
+                else {
+                    Object.keys(d.scores).forEach(function(score){
+                        if(!_this.scoreSetDict[score])
+                            _this.scoreSetDict[score] = { frequency: 0, max: 0 };
+                        _this.scoreSetDict[score].frequency++;
+                        _this.scoreSetDict[score].max = d.scores[score] > _this.scoreSetDict[score].max ? d.scores[score]: _this.scoreSetDict[score].max;
+                    });
+                }
+            });
+
+            Object.keys(_this.scoreSetDict).forEach(function(score){
+                _this.getScoreSet.push({ score: score, frequency: _this.scoreSetDict[score].frequency, max: _this.scoreSetDict[score].max });
+            });
+
+            // get normalized scores
+            this.normalizedScores = [];
+
+            this.items.forEach(function(item){});
+
+        },
+
+        getNormalizedItemScores: function(){
+            return this.normalizedScores;
+        },
+
+        getScoreSet: function(){
+            return { dict: _this.scoreSetDict, array: _this.scoreSet };
+        }
+
+    };
+
+    return ScoreExtractor;
+})();
--- test/server/error.php
+++
@@ -1,8 +0,0 @@
-<?php
-function return_error($message, $code) {
-    header('HTTP/1.1 500 Internal Server Error -- '.$message);
-    header('Content-Type: application/json; charset=UTF-8');
-    die(json_encode(array('message' => 'ERROR '.$message, 'code' => $code)));
-    echo 'ERROR -- '.$message;
-}
-?>
