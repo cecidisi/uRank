8f6572a7a23f5dc3825ac0dc618be9df9afd9b3d
MeSantokh <ssingh@know-center.at>
2015-11-11 18:51:35 +0100

    [urank] fixing configurable keywordextractor

--- urank.js
+++ urank.js
@@ -152,13 +152,16 @@ var Urank = (function(){
 	            _this.data.forEach(function(d, i){
 	                d.keywords = keywordExtractor.listDocumentKeywords(i);
 	            });
-	            o.keywordExtractor.keywords = keywordExtractor.getCollectionKeywords();
-	        	o.keywordExtractor.keywordsDict = keywordExtractor.getCollectionKeywordsDictionary();
+	            o.keywordExtractor.extractedData.keywords = keywordExtractor.getCollectionKeywords();
+	        	o.keywordExtractor.extractedData.keywordsDict = keywordExtractor.getCollectionKeywordsDictionary();
+	        }
+	        else {
+	        	 o.keywordExtractor = options.keywordExtractor; 
 	        }
 
             //  Assign collection keywords and set other necessary variables
-            _this.keywords =  o.keywordExtractor.keywords; 
-            _this.keywordsDict = o.keywordExtractor.keywordsDict; 
+            _this.keywords =  o.keywordExtractor.extractedData.keywords; 
+            _this.keywordsDict = o.keywordExtractor.extractedData.keywordsDict; 
             _this.rankingMode = RANKING_MODE.overall_score;
             _this.rankingModel.clear().setData(_this.data);
             _this.selectedKeywords = [];
